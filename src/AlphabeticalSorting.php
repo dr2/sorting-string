687b3aede9da5612b9af490b93e252f86de2d112
dr2 <novikovxkv@gmail.com>
2018-11-21 19:42:23 +0300

    Start version

---
+++ src/AlphabeticalSorting.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Class SortingString
+ */
+
+namespace TestTask\SortingString;
+
+class AlphabeticalSorting
+{
+
+
+    /**
+     * Alphabetical word sorting
+     *
+     * @param string $string
+     * @return string
+     */
+    public function sortString($string)
+    {
+
+        $process_arr = [];
+
+        $array_words = explode(' ', $string);
+
+        foreach ($array_words as $word) {
+            $process_arr[] = $this->sortLetter($word);
+        }
+
+        return implode(' ', $process_arr);
+    }
+
+
+    /**
+     * Sort letters in a word
+     *
+     * @param string $word
+     * @return string
+     */
+    protected function sortLetter($word)
+    {
+        $array_letter = preg_split('//u', $word, -1, PREG_SPLIT_NO_EMPTY);
+
+        return implode($this->sortArray($array_letter));
+    }
+
+
+    /**
+     * Sorting an array of letters in ascending order
+     *
+     * @param array $array_letters
+     * @return array
+     */
+    protected function sortArray($array_letters)
+    {
+        $array_lower = array_map('mb_strtolower', $array_letters);
+        array_multisort($array_lower, SORT_ASC, SORT_STRING, $array_letters);
+
+        return $array_letters;
+    }
+
+}
---
+++ tests/AlphabeticalSortingTest.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+use TestTask\SortingString\AlphabeticalSorting;
+use PHPUnit\Framework\TestCase;
+
+class AlphabeticalSortingTest extends TestCase
+{
+    private $calculator;
+
+    protected function setUp()
+    {
+        $this->calculator = new AlphabeticalSorting();
+    }
+
+    protected function tearDown()
+    {
+        $this->calculator = NULL;
+    }
+
+    public function additionProvider() {
+        return [
+            'eng string' => ['lemon orange banana apple', 'elmno aegnor aaabnn aelpp'],
+            'ru string' => ['лимон апельсин банан яблоко', 'илмно аеилнпсь аабнн бклооя'],
+            'latin string' => ['αβγαβγ αβγαβγαβγ', 'ααββγγ αααβββγγγ']
+        ];
+    }
+
+
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testAdd($string, $expected)
+    {
+        $result = $this->calculator->sortString($string);
+        $this->assertEquals($expected, $result);
+    }
+}
